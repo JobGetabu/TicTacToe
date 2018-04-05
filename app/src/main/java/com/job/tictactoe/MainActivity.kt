f2eb15cdcd3a51d686231bdb9d5d853c8bddb536
JobGetabu <getabujob@gmail.com>
2018-04-06 01:03:24 +0300

    fixed #bug26 "intent backstack issues"

--- app/src/main/java/com/job/tictactoe/MainActivity.kt
+++ app/src/main/java/com/job/tictactoe/MainActivity.kt
@@ -3,38 +3,44 @@ package com.job.tictactoe
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.Button
-import butterknife.BindView
-import butterknife.ButterKnife
-import butterknife.OnClick
 
 class MainActivity : AppCompatActivity() {
 
 
-    @BindView(R.id.main_player1) val btnPlayer1 : Button? = null
-    @BindView(R.id.main_player2) val btnPlayer2 : Button? = null
+     private var btnPlayer1 : Button? = null
+     private var btnPlayer2 : Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        ButterKnife.bind(this)
+        btnPlayer1 = findViewById(R.id.main_player1);
+        btnPlayer2 = findViewById(R.id.main_player2);
+
+        btnPlayer1?.setOnClickListener { view -> player1Click(view) }
+        btnPlayer2?.setOnClickListener { view -> player2Click(view) }
 
     }
 
-    @OnClick(R.id.main_player1)
-    public fun player1Click(){
-        val longGameIntent = Intent(this@MainActivity, LongGameActivity::class.java)
-        longGameIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        startActivity(longGameIntent)
-        finish()
-    }
 
-    @OnClick(R.id.main_player2)
-    public fun player2Click(){
+    private fun player1Click(view : View){
+
+        //TODO carry boolean is 1 player
         val shortGameIntent = Intent(this@MainActivity, GameActivity::class.java)
         shortGameIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         startActivity(shortGameIntent)
-        finish()
+       /* val longGameIntent = Intent(this@MainActivity, LongGameActivity::class.java)
+        longGameIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(longGameIntent)*/
+    }
+
+
+    private fun player2Click(view : View){
+        //TODO carry boolean is 1 player
+        val shortGameIntent = Intent(this@MainActivity, GameActivity::class.java)
+        shortGameIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(shortGameIntent)
     }
 }
